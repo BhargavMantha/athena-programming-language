593aba694bfd5204d54eed1b2e50305466d8185b
BhargavMantha <manthabhargav@gmail.com>
2021-04-12 03:45:28 +0530

    Added 1. lexer 2. dectonary

---
+++ src/athena-lexer/athena-lexer.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@nestjs/common';
+import { IllegalCharacterError } from 'src/athena-error/athena-error.service';
+import { AthenaTokenService } from 'src/athena-token/athena-token.service';
+import { athenaTypeOfDigit, lexerDictionary } from './dictionary';
+
+export class AthenaLexerService {
+  self: { text: string[]; position: number; currentCharacter: string } = {
+    text: [],
+    position: -1,
+    currentCharacter: undefined
+  };
+
+  constructor(text: string) {
+    this.self.text = text.split('');
+    this.nextCharacter();
+  }
+  nextCharacter() {
+    this.self.position += 1;
+    this.self.currentCharacter =
+      this.self.position < this.self.text.length
+        ? this.self.text[this.self.position]
+        : undefined;
+  }
+  elementInChecker(elementTotCheckAgainst: string[], element: any) {
+    return elementTotCheckAgainst.some(
+      (eachElement) => eachElement === element
+    );
+  }
+  makeTokens() {
+    const tokens = [];
+    while (this.self.currentCharacter !== undefined) {
+      if (this.elementInChecker([...'\t '], this.self.currentCharacter)) {
+        this.nextCharacter();
+      } else if (
+        this.elementInChecker(
+          [...lexerDictionary.DIGITS],
+          this.self.currentCharacter
+        )
+      ) {
+        tokens.push(this.makeNumbers());
+        this.nextCharacter();
+      } else if (lexerDictionary[this.self.currentCharacter]) {
+        tokens.push(lexerDictionary[this.self.currentCharacter]);
+        this.nextCharacter();
+      } else {
+        const char = this.self.currentCharacter;
+        this.nextCharacter();
+        return { elements: [], error: new IllegalCharacterError(`${char}`) };
+      }
+    }
+    return { elements: tokens, error: undefined };
+  }
+  makeNumbers(): AthenaTokenService {
+    let numStr = '';
+    let dotCount = 0;
+    while (
+      this.self.currentCharacter !== undefined &&
+      this.elementInChecker(
+        [...lexerDictionary.DIGITS],
+        this.self.currentCharacter
+      )
+    ) {
+      if (this.self.currentCharacter === '.') {
+        if (dotCount === 1) {
+          break;
+        }
+        dotCount += 1;
+        numStr += '.';
+      } else {
+        numStr += this.self.currentCharacter;
+      }
+      this.nextCharacter();
+    }
+    if (dotCount == 0) {
+      return new AthenaTokenService(athenaTypeOfDigit.int, parseInt(numStr));
+    } else {
+      return new AthenaTokenService(
+        athenaTypeOfDigit.float,
+        parseFloat(numStr)
+      );
+    }
+  }
+}
---
+++ src/athena-lexer/dictionary.ts
@@ -0,0 +1,14 @@
+export const lexerDictionary = {
+  DIGITS: '0123456789.',
+  '+': 'AT_PLUS',
+  '-': 'AT_MINUS',
+  '*': 'AT_MULTIPLY',
+  '/': 'AT_DIVIDE',
+  '(': 'AT_LEFT_PARENTHESIS',
+  ')': 'AT_RIGHT_PARENTHESIS'
+};
+
+export const athenaTypeOfDigit = {
+  int: 'AT_INTEGER',
+  float: 'AT_FLOAT'
+};
