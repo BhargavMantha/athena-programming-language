c9abf319682156784bb8a3e8c45adfdd7da1a490
BhargavMantha <manthabhargav@gmail.com>
2021-04-12 03:47:35 +0530

    standalone mode

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InfinitePrompterModule } from './infinite-prompter/infinite-prompter.module';
+import { AthenaErrorService } from './athena-error/athena-error.service';
 
 @Module({
-  imports: [],
+  imports: [InfinitePrompterModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AthenaErrorService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { InfinitePrompterService } from './infinite-prompter/infinite-prompter.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const infinitePrompterService = app.get(InfinitePrompterService);
+  await infinitePrompterService.initiate();
 }
 bootstrap();
