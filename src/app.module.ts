45269b0a603cc0fe4396e11440136c6fac6849d5
BhargavMantha <manthabhargav@gmail.com>
2021-08-29 10:56:35 +0530

    removed unnecessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { InfinitePrompterModule } from './infinite-prompter/infinite-prompter.module';
 import { AthenaErrorService } from './athena-error/athena-error.service';
 
 @Module({
   imports: [InfinitePrompterModule],
-  controllers: [AppController],
-  providers: [AppService, AthenaErrorService]
+  providers: [AthenaErrorService]
 })
 export class AppModule {}
