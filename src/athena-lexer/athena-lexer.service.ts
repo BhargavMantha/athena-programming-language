f9eececb1b151d65afe1518d8c771f5d6246d77d
BhargavMantha <manthabhargav@gmail.com>
2021-08-29 13:44:31 +0530

    1. changed the variable name 2. removed the unnecessary .nextCharacter hop

--- src/athena-lexer/athena-lexer.service.ts
+++ src/athena-lexer/athena-lexer.service.ts
@@ -5,23 +5,27 @@ import { IToken } from 'src/types';
 import { athenaTypeOfDigit, lexerDictionary } from './dictionary';
 
 export class AthenaLexerService {
-  self: { text: string[]; position: number; currentCharacter: string } = {
-    text: [],
+  expression: {
+    textArray: string[];
+    position: number;
+    currentCharacter: string;
+  } = {
+    textArray: [],
     position: -1,
     currentCharacter: undefined
   };
 
   constructor(text: string) {
-    this.self.text = text.split('');
+    this.expression.textArray = [...text];
     this.nextCharacter();
   }
-  nextCharacter() {
-    this.self.position += 1;
-    this.self.currentCharacter =
-      this.self.position < this.self.text.length
-        ? this.self.text[this.self.position]
+  nextCharacter = () => {
+    this.expression.position += 1;
+    this.expression.currentCharacter =
+      this.expression.position < this.expression.textArray.length
+        ? this.expression.textArray[this.expression.position]
         : undefined;
-  }
+  };
   elementInChecker(elementTotCheckAgainst: string[], element: any) {
     return elementTotCheckAgainst.some(
       (eachElement) => eachElement === element
@@ -29,26 +33,27 @@ export class AthenaLexerService {
   }
   makeTokens() {
     const tokens: IToken[] = [];
-    while (this.self.currentCharacter !== undefined) {
-      if (this.elementInChecker([...'\t '], this.self.currentCharacter)) {
+    while (this.expression.currentCharacter !== undefined) {
+      if (this.elementInChecker([...'\t '], this.expression.currentCharacter)) {
         this.nextCharacter();
       } else if (
         this.elementInChecker(
           [...lexerDictionary.DIGITS],
-          this.self.currentCharacter
+          this.expression.currentCharacter
         )
       ) {
         tokens.push(this.makeNumbers());
-        this.nextCharacter();
-      } else if (lexerDictionary[this.self.currentCharacter]) {
+      } else if (
+        lexerDictionary.hasOwnProperty(this.expression.currentCharacter)
+      ) {
         const operationToken = new AthenaTokenService(
           athenaTypeOfDigit.operation,
-          lexerDictionary[this.self.currentCharacter]
+          lexerDictionary[this.expression.currentCharacter]
         ).returnToken();
         tokens.push(operationToken);
         this.nextCharacter();
       } else {
-        const char = this.self.currentCharacter;
+        const char = this.expression.currentCharacter;
         this.nextCharacter();
         return { elements: [], error: new IllegalCharacterError(`${char}`) };
       }
@@ -59,20 +64,20 @@ export class AthenaLexerService {
     let numStr = '';
     let dotCount = 0;
     while (
-      this.self.currentCharacter !== undefined &&
+      this.expression.currentCharacter !== undefined &&
       this.elementInChecker(
         [...lexerDictionary.DIGITS],
-        this.self.currentCharacter
+        this.expression.currentCharacter
       )
     ) {
-      if (this.self.currentCharacter === '.') {
+      if (this.expression.currentCharacter === '.') {
         if (dotCount === 1) {
           break;
         }
         dotCount += 1;
         numStr += '.';
       } else {
-        numStr += this.self.currentCharacter;
+        numStr += this.expression.currentCharacter;
       }
       this.nextCharacter();
     }
